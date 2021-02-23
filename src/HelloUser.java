cec756cea0ccc49e100ce26f96d77dac3d05cb6f
Marlen Zhumakov <mzhumakov@gmail.com>
2021-02-23 23:27:30 +0600

    add some text

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -4,9 +4,9 @@ public class HelloUser {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Type your name under this line, please:");
+        System.out.println("Dear User, type your name under this line, please:");
         String userName = input.nextLine();
 
-        System.out.println("Hello dear, " + userName + "!");
+        System.out.println("Hello, " + userName + "!");
     }
 }
