882645fec7d15c91c8f1fe57ad705b1dd41b7a05
Marlen Zhumakov <mzhumakov@gmail.com>
2021-02-24 13:56:06 +0600

    change code with possibility to add several names

--- src/HelloUser.java
+++ src/HelloUser.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class HelloUser {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Dear User, type your name under this line, please:");
-        String userName = input.nextLine();
-
-        System.out.println("Hello, " + userName + "!");
+        //Scanner input = new Scanner(System.in);
+        //System.out.println("Dear User, type your name under this line, please:");
+        //String userName = input.nextLine();
+        for (String names : args) {
+            System.out.println("Hello, " + names + "!");
+        }
     }
 }
