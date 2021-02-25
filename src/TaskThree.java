1925800bf79c6dd09bc0422740d4740dc5dcf47f
Marlen Zhumakov <mzhumakov@gmail.com>
2021-02-25 22:40:51 +0600

    create task 3 solution

---
+++ src/TaskThree.java
@@ -0,0 +1,11 @@
+public class TaskThree {
+    public static void main(String[] args) {
+        for (String number : args) {
+            System.out.print(number + " ");
+        }
+        System.out.println("");
+        for (String number : args) {
+            System.out.println(number);
+        }
+    }
+}
